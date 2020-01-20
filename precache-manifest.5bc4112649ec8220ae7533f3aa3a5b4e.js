f8e7a2e63c19ff22ff7f49c55fd2d365f3eceeab
Oluwakayode <fadojuk@gmail.com>
2020-01-20 03:29:55 +0100

    Updates

---
+++ precache-manifest.5bc4112649ec8220ae7533f3aa3a5b4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1bbed40a8c73282b43b2d2218400c87",
+    "url": "/got-quotes/index.html"
+  },
+  {
+    "revision": "a338cfb547d599100ccd",
+    "url": "/got-quotes/static/css/main.142a3d65.chunk.css"
+  },
+  {
+    "revision": "a70a9ed6c256cd283022",
+    "url": "/got-quotes/static/js/2.b2cc98db.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/got-quotes/static/js/2.b2cc98db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a338cfb547d599100ccd",
+    "url": "/got-quotes/static/js/main.20f03752.chunk.js"
+  },
+  {
+    "revision": "9cacfb261a8dcaad9f52",
+    "url": "/got-quotes/static/js/runtime-main.26766a7a.js"
+  },
+  {
+    "revision": "8c9af1ae616377aacff08431f8d87b8b",
+    "url": "/got-quotes/static/media/aryastark.8c9af1ae.jpg"
+  }
+]);
